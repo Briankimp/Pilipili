451108703fc8feb1faa90c17610a9edbe8b6f2d1
Briankimp <bkimathi973@gmail.com>
2024-09-04 02:29:48 +0300

    This is th first commit

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,49 @@
+// import path from 'path';
+import Link from 'next/link';
+import React from 'react'
+
+
+const Navlinks = [
+  {
+    name: "Home",
+    path: "/home",
+  },
+  {
+    name: "Ingredients",
+    path: "/Ingredients",
+  },
+  {
+    name: "Uses",
+    path: "/Uses",
+  },
+  {
+    name: "Shop",
+    path: "/Shop",
+  },
+  {
+    name: "Contact",
+    path: "/contacts",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="text-lg text-text-black font-semibold  p-3 pt-3 align-middle">
+      <div className="flex justify-between ">
+        <div>
+          <h1 className='text-4xl' >22</h1>
+        </div>
+        <div className='text-lg gap-3 p-3  '>
+          {Navlinks.map((Navlink, index) => (
+            <Link
+              key={index}
+              href={Navlink.path}
+              className='p-3'>{Navlink.name}</Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+    },
+    colors: {
+      green: {
+        100: "#244747",
       },
+      black: {
+        100: "#000000", // Fixed color code
+      },
+      bg: "#fff6e4", // Fixed extra space
+      btn: {
+        red: "#d93430",
+        yellow: "#ff811a",
+        orange: "#ffde59",
+      },
+    },
+    fontFamily: {
+      primary: ["Nunito", "sans-serif"], // Ensure 'Nunito' is used in your global CSS
     },
   },
   plugins: [],
 };
+
 export default config;
