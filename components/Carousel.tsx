da08aab50ece4306934968ff83ffdbb628e948d4
Briankimp <bkimathi973@gmail.com>
2024-09-20 00:56:32 +0300

    Changes to enable Deployment

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,51 +5,76 @@ import { Card } from "antd";
 
 interface Recipe {
   ingredients: string;
-  instructions: string;
+  instructions: Array<string>; // Change this to Array<string> | string
   name: string;
 }
 const recipes: Recipe[] = [
   {
     name: "Hot Pizza",
     ingredients: "22 Chilli, ginger",
-    instructions:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nisi ullam <br/> sed, mollitia id commodi pariatur cumque eos a repudiandae tempora <br/> impedit expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    instructions: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      "Laborum nisi ullam sed, mollitia id commodi pariatur",
+      "cumque eos a repudiandae tempora<br /> impedit",
+      "expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    ],
   },
   {
     name: "Hote Pizza",
     ingredients: "22 Chilli, ginger",
-    instructions:
-      "Step 1: Lorem ipsum dolor sit, Step 2: amet consectetur adipisicing elit, Step 3: Laborum nisi ullam brsed, Step 4: Stepmollitia id commodi pariatur cumque eos, Step 5: a repudiandae tempora impedit expedita quis, Step 6: illum dolorum ipsa perferendis nostrum natus.",
+    instructions: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      "Laborum nisi ullam sed, mollitia id commodi pariatur",
+      "cumque eos a repudiandae tempora<br /> impedit",
+      "expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    ],
   },
   {
     name: "Hoto Pizza",
     ingredients: "22 Chilli, ginger",
-    instructions:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nisi ullam <br/> sed, mollitia id commodi pariatur cumque eos a repudiandae tempora <br/> impedit expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    instructions: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      "Laborum nisi ullam sed, mollitia id commodi pariatur",
+      "cumque eos a repudiandae tempora<br /> impedit",
+      "expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    ],
   },
   {
     name: "Hot Pizzwa",
     ingredients: "22 Chilli, ginger",
-    instructions:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nisi ullam <br/> sed, mollitia id commodi pariatur cumque eos a repudiandae tempora <br/> impedit expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    instructions: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      "Laborum nisi ullam sed, mollitia id commodi pariatur",
+      "cumque eos a repudiandae tempora<br /> impedit",
+      "expedita quis illum dolorum ipsa perferendis nostrum natus.",
+    ],
   },
 ];
 
 const CarouselComponent = () => {
   return (
     <div className=" w-full bg-white-100 ">
-      <Carousel autoplay className=" w-full bg-white-100 ">
+      <Carousel
+        autoplay
+        className=" w-full bg-white-100 "
+        dots={false}
+        arrows={true}
+      >
         {recipes.map((recipe, index) => (
           <div
             key={index}
             className="w-full border shadow-sm flex-col-1 text-center bg-white-100"
           >
             {/* <Card title="Born"> */}
-            <Card title={recipe.name}>
-              {/* <h1 className="text-2xl font-bold">{recipe.name}</h1> */}
-              <h2 className="text-xl text-wrap">{recipe.ingredients}</h2>
-              <h2 className="text-xl text-wrap">{recipe.instructions}</h2>
-            </Card>
+            {/* <Card title={recipe.name}> */}
+            <h1 className="text-2xl font-bold">{recipe.name}</h1>
+            <h2 className="text-xl text-wrap">{recipe.ingredients}</h2>
+            <ol>
+              {recipe.instructions.map((instruction: string, index: number) => (
+                <li key={index}>{instruction}</li>
+              ))}
+            </ol>
+            {/* </Card> */}
           </div>
         ))}
       </Carousel>
