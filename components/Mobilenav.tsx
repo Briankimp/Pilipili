a63f92640a2deb5c8b349179e365cca6a51dbb67
Briankimp <bkimathi973@gmail.com>
2024-09-20 03:19:47 +0300

    Better Now

--- components/Mobilenav.tsx
+++ components/Mobilenav.tsx
@@ -7,7 +7,7 @@ import Button from "./Button";
 const Navlinks = [
   {
     name: "Home",
-    path: "/home",
+    path: "/",
   },
   {
     name: "Ingredients",
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import Mobilenav from "./Mobilenav";
 const Navlinks = [
   {
     name: "Home",
-    path: "/home",
+    path: "/",
   },
   {
     name: "Ingredients",
