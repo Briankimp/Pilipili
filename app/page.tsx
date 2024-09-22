502cdb25bd447097b0235119f15c014ac82b3d2f
Briankimp <bkimathi973@gmail.com>
2024-09-22 16:43:32 +0300

    hover colour changed

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Button from "../components/Button";
 import Shop from "./Shop/page";
 import Contact from "./contact/page";
 import Ingredients from "./Ingredients/page";
+import { motion } from "framer-motion";
 
 const Home: React.FC = () => {
   return (
@@ -14,38 +15,50 @@ const Home: React.FC = () => {
       <div className="sticky top-0">
         <Navbar />
       </div>
-      <div className="text-green-100 w-full flex flex-col gap-9 h-screen p-6">
-        <h1 className="pt-9 text-6xl md:text-8xl font-semibold items-center">
-          <span>22</span> Chilli Flakes
-        </h1>
-        <h1 className="text-2xl italic font-light">Spice Life Naturally</h1>
-        <h2 className="text-xl">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nisi
-          ullam <br />
-          sed, mollitia id commodi pariatur cumque eos a repudiandae tempora{" "}
-          <br />
-          impedit expedita quis illum dolorum ipsa perferendis nostrum natus.
-        </h2>
-        <div className="flex w-full text-lg items-start">
-          <Link href="/">
-            <Button variant="Primary" className="items-center flex">
-              <span>Shop in Kenya</span>
-            </Button>
-          </Link>
-          <Link href="/">
-            <Button variant="Primary" className="items-center flex">
-              <span>Shop in Rwanda</span>
-            </Button>
-          </Link>
+
+      {/* Parallax Section */}
+      <div className="relative h-screen overflow-hidden">
+        <motion.div
+          className="" // Change to desired background color
+          initial={{ y: 0 }}
+          whileInView={{ y: -100 }} // Adjust for effect
+          transition={{ duration: 1, ease: "easeInOut" }}
+        />
+        <div className="relative z-10 flex flex-col gap-9 h-full p-6 text-green-100">
+          <h1 className="pt-9 text-6xl md:text-8xl font-semibold items-center">
+            <span>22</span> Chilli Flakes
+          </h1>
+          <h1 className="text-2xl italic font-light">Spice Life Naturally</h1>
+          <h2 className="text-xl">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+            nisi ullam <br />
+            sed, mollitia id commodi pariatur cumque eos a repudiandae tempora{" "}
+            <br />
+            impedit expedita quis illum dolorum ipsa perferendis nostrum natus.
+          </h2>
+          <div className="flex w-full text-lg items-start">
+            <Link href="/">
+              <Button variant="Primary" className="items-center flex">
+                <span>Shop in Kenya</span>
+              </Button>
+            </Link>
+            <Link href="/">
+              <Button variant="Primary" className="items-center flex">
+                <span>Shop in Rwanda</span>
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
+
+      {/* Other Sections */}
       <div className="h-screen w-full text-green-100">
         <Ingredients />
       </div>
       <div className="h-screen">
         <Uses />
       </div>
-      <div className="h-screen bg-cover bg-center bg-white-200">
+      <div className="h-screen bg-white-200">
         <Shop />
       </div>
       <div className="px-4">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,19 +35,21 @@ const Navbar = () => {
   return (
     <div className="sticky  ">
       <div className=" hidden md:flex justify-between mx-1 text-lg rounded-full mt-3 bg-white-200 shadow-sm text-text-black font-semibold p-3 align-middle">
-
-          <div>
-            <h1 className="text-5xl font-bold text-black-100">22</h1>
-          </div>
-          <div className="text-lg text-green-100 hover:text-black-100 gap-3 p-3  ">
-            {Navlinks.map((Navlink, index) => (
-              <Link key={index} href={Navlink.path} className="p-3">
-                {Navlink.name}
-              </Link>
-            ))}
-
+        <div>
+          <h1 className="text-5xl font-bold text-black-100">22</h1>
         </div>
+        <div className="text-lg gap-3 p-3  ">
+          {Navlinks.map((Navlink, index) => (
+            <Link
+              key={index}
+              href={Navlink.path}
+              className="  text-green-100 hover:text-btn-yellow p-3"
+            >
+              {Navlink.name}
+            </Link>
+          ))}
         </div>
+      </div>
 
       {/* // Mobile Nav */}
 
